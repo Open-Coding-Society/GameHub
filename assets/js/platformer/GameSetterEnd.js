0bf8ad415dc4c9181c521be2d28cb3f354936f3c
im2008 <ianm02879@gmail.com>
2025-04-30 22:44:27 -0700

    test 2

--- assets/js/platformer/GameSetterEnd.js
+++ assets/js/platformer/GameSetterEnd.js
@@ -4,7 +4,7 @@ import Background from './Background.js'
 // Define the assets
 const assets = {  
   backgrounds: {
-    end: { src: "/images/platformer/backgrounds/scripps_starting.png" },
+    end: { src: "/images/platformer/backgrounds/scripps_startings.png" },
   },
 };
 
